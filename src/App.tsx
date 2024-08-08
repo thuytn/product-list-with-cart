5bd038048f35ffa4162aac660fba5fc431d8a798
thuytn <tranthinhuthuyd1tp@gmail.com>
2024-08-08 11:46:42 +0700

    fix err

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import ShoppingCart from "./components/ShoppingCart.tsx";
 function App() {
   return (
     <div className="relative min-h-screen bg-rose-50 flex items-center justify-center">
-      <main className="flex w-full flex-col gap-10 p-6 md:flex-row md:justify-between md:items-center md:gap-8 xl:max-w-[1220px] xl:gap-10">
+      <main className="flex w-full flex-col gap-10 min-h-screen p-6 md:flex-row md:justify-between md:items-center md:gap-8 xl:max-w-[1220px] xl:gap-10">
         <Store />
         <ShoppingCart />
       </main>
